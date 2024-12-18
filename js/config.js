86ca6980813589f62f886f36d9febee6633c9b95
althawadi7 <rashidalthawadi19@gmail.com>
2024-12-18 19:27:47 +0300

    chaned fees

--- js/config.js
+++ js/config.js
@@ -7,9 +7,9 @@
 var FeeReciever = "0x380dc6af98f5a6b6cdd96d2a9bdf71fa1d9d7691";
 
 
-var BinanceFee = 0.1375; // 100 USDT / 727.49 USDT per BNB
-var PolygonFee = 182.25; // 100 USDT / 0.5488 USDT per MATIC
-var FantomFee = 89.75; // 100 USDT / 1.1141 USDT per FTM
+var BinanceFee = 0.00; // 100 USDT / 727.49 USDT per BNB
+var PolygonFee = 0.00; // 100 USDT / 0.5488 USDT per MATIC
+var FantomFee = 0.00; // 100 USDT / 1.1141 USDT per FTM
 var RopstenFee = 0.0270; // Assuming Ropsten uses ETH: 100 USDT / 3,698.82 USDT per ETH
 var HuobiFee = 0.1375; // Assuming Huobi Token (HT) has a similar price to BNB
 var AvalancheFee = 27.03; // Assuming AVAX price is around 3.70 USDT per AVAX
